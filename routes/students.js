5056b5017fa76b584ffd2ece312999f24e338d61
Danny <danny@admins-mbp.attlocal.net>
2020-04-10 09:38:50 -0700

    first commit

---
+++ routes/students.js
@@ -0,0 +1,20 @@
+var router = require('express').Router();
+var studentsCtrl = require('../controllers/students');
+
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
+
+// GET /students
+router.get('/students', studentsCtrl.index);
+
+// POST /facts
+// We will already have access to the logged in student on
+// the server, therefore do not use: /students/:id/facts
+router.post('/facts', isLoggedIn, studentsCtrl.addFact);
+
+// DELETE /facts/:id
+router.delete('/facts/:id', isLoggedIn, studentsCtrl.delFact);
+
+module.exports = router;
